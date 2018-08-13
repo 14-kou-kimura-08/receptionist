ecdf40639889decb8f0d3e2f19b30b4bc5d0da3c
Kou <kimura@kurashicom.jp>
2018-08-13 20:41:20 +0900

    create migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('last_furigana');
+            $table->string('first_furigana');
+            $table->integer('group_id');
+            $table->string('slack_name');
             $table->rememberToken();
             $table->timestamps();
         });
