86f674c7e17d4e2646a721d522cc80e9d880345b
Kou <kimura@kurashicom.jp>
2018-08-15 17:29:53 +0900

    adjust each view

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -1,3 +1,19 @@
-@foreach ($groups as $group)
-    <a href="{{ route('users.index', ['group_id' => $group->id]) }}">{{$group->name}}</a>
-@endforeach
+@extends('layouts.layouts')
+
+@section('css', '/css/groups.css')
+
+@section('content')
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                担当者の所属グループを教えてください
+            </div>
+            <div class="links">
+                @foreach ($groups as $group)
+                    <a href="{{ route('users.index', ['group_id' => $group->id]) }}">{{$group->name}}</a>
+                @endforeach
+            </div>
+            <a href="{{ route('welcome') }}" class="return">１つ前に戻る</a>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/layouts.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+    <head>
+        <title>receptionist</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/layouts.css">
+        <link rel="stylesheet" href="@yield('css')">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        
+        @yield('head')
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
