abde800af58309fff2fa53bd82ddaacc1363a321
Kou <kimura@kurashicom.jp>
2018-08-25 22:13:33 +0900

    delete UsersController method

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,52 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function __construct(User $user)
-    {
-        $this->user = $user;
-    }
-
-    /**
-     * Index the users.
-     *
-     *
-     * @return Response
-     */
-    public function index($group_id)
-    {
-        $users = $this->user->where('group_id', $group_id)->get();
-        return view('users.index', ['users' => $users]);
-    }
-
-    /**
-     * Notification to Specific User.
-     *
-     *
-     * @return Response
-     */
-    public function notification($id)
-    {
-        $user = $this->user->find($id);
-        $user->notify(new \App\Notifications\CallFromReceptionist($user));
-        return view('thanks');
-    }
-
-    /**
-     * Notification to Corporate Group.
-     *
-     *
-     * @return Response
-     */
-    public function notificationToCorporate()
-    {
-        $id   = $this->user->where('last_name', '木村')->value('id');
-        $user = $this->user->find($id);
-        $user->notify(new \App\Notifications\CallFromReceptionist($user));
-        return view('thanks');
-    }
+    //
 }
