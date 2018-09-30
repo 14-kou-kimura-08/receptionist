ab6f7421656f13bab761794ccb959a287c2f15df
Kou <kimura@kurashicom.jp>
2018-09-30 10:07:52 +0900

    Change Seeder

--- database/seeds/GroupsTableSeeder.php
+++ database/seeds/GroupsTableSeeder.php
@@ -13,13 +13,14 @@ class GroupsTableSeeder extends Seeder
     public function run()
     {
         Group::insert([
-            ["id" => 1, "name" => "テクノロジー", "slack_name" => "", ],
-            ["id" => 2, "name" => "デザイン", "slack_name" => "", ],
-            ["id" => 3, "name" => "事業開発", "slack_name" => "", ],
+            ["id" => 1, "name" => "役員", "slack_name" => "", ],
+            ["id" => 2, "name" => "エディトリアル", "slack_name" => "", ],
+            ["id" => 3, "name" => "MD", "slack_name" => "", ],
             ["id" => 4, "name" => "コーポレート", "slack_name" => "", ],
-            ["id" => 5, "name" => "コミュニケーション", "slack_name" => "", ],
-            ["id" => 6, "name" => "MD", "slack_name" => "", ],
-            ["id" => 7, "name" => "エディトリアル", "slack_name" => "", ],
+            ["id" => 5, "name" => "デザイン", "slack_name" => "", ],
+            ["id" => 6, "name" => "事業開発", "slack_name" => "", ],
+            ["id" => 7, "name" => "コミュニケーション", "slack_name" => "", ],
+            ["id" => 8, "name" => "テクノロジー", "slack_name" => "", ],
         ]);
     }
 }
