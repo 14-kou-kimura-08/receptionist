33dd8fe65e5b9a986ab0d2918ef2ab68b33d6708
Kou <kimura@kurashicom.jp>
2018-10-10 14:05:03 +0900

    Add slack name to Group Seeder

--- database/seeds/GroupsTableSeeder.php
+++ database/seeds/GroupsTableSeeder.php
@@ -13,14 +13,14 @@ class GroupsTableSeeder extends Seeder
     public function run()
     {
         Group::insert([
-            ["id" => 1, "name" => "役員", "slack_name" => "", ],
-            ["id" => 2, "name" => "エディトリアル", "slack_name" => "", ],
-            ["id" => 3, "name" => "MD", "slack_name" => "", ],
-            ["id" => 4, "name" => "コーポレート", "slack_name" => "", ],
-            ["id" => 5, "name" => "デザイン", "slack_name" => "", ],
-            ["id" => 6, "name" => "事業開発", "slack_name" => "", ],
-            ["id" => 7, "name" => "コミュニケーション", "slack_name" => "", ],
-            ["id" => 8, "name" => "テクノロジー", "slack_name" => "キムプロテスト", ],
+            ["id" => 1, "name" => "役員", "slack_name" => "gr_corp", ],
+            ["id" => 2, "name" => "エディトリアル", "slack_name" => "gr_edi", ],
+            ["id" => 3, "name" => "MD", "slack_name" => "gr_md", ],
+            ["id" => 4, "name" => "コーポレート", "slack_name" => "gr_corp", ],
+            ["id" => 5, "name" => "デザイン", "slack_name" => "gr_design", ],
+            ["id" => 6, "name" => "事業開発", "slack_name" => "gr_bdev", ],
+            ["id" => 7, "name" => "コミュニケーション", "slack_name" => "gr_comm", ],
+            ["id" => 8, "name" => "テクノロジー", "slack_name" => "gr_tech", ],
         ]);
     }
 }
